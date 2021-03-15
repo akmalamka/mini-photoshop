366bf79da93ee819797970e6f39d777f5cd5858f
Muhammad Akmal <akmalm@Muhammads-MacBook-Pro.local>
2021-03-15 11:28:52 +0700

    update menubar

--- src/ui/mainUI.py
+++ src/ui/mainUI.py
@@ -21,14 +21,18 @@ class App(Frame):
         self.booleanMenu = Menu(self.editMenu, tearoff=False)
         self.geometryMenu = Menu(self.editMenu, tearoff=False)
         self.histogramMenu = Menu(self.editMenu, tearoff=False)
+        self.rotationMenu = Menu(self.editMenu, tearoff=False)
+        self.flipMenu = Menu(self.editMenu, tearoff=False)
+        self.zoomMenu = Menu(self.editMenu, tearoff=False)
         self.filename = ''
         self.status = 'Initializing'
         self.statusbar = Label(self.parent, text = self.status)
         self.statusbar.pack(side=BOTTOM, fill=X)
-        self.imageArr = np.zeros((1,1), dtype=np.uint8)
+        self.imageArrMain = np.zeros((1,1), dtype=np.uint8) #array image utama
+        self.imageArrOperation = np.zeros((1,1), dtype=np.uint8) #array image yang akan dioperasikan
         self.scalarValue = DoubleVar()
         self.strVar = StringVar()
-        self.id = IntVar()
+        self.id = IntVar() #id command yang diberikan
         self.value = Label(self.parent, textvariable= self.strVar)
         self.initUI()
 
@@ -39,32 +43,43 @@ class App(Frame):
         self.fileMenu.add_command(label="Save File", command = lambda: self.save_image("Save File"), state='disabled')
         self.menubar.add_cascade(label="File", menu=self.fileMenu)
 
-        self.editMenu.add_command(label="Negative", command= lambda: self.negative("Negative"))
-        self.editMenu.add_command(label="Grayscale", command=lambda: self.grayscale("Grayscale"))
+        self.editMenu.add_command(label="Negative", command= lambda: self.negative("Negative"), state='disabled')
+        self.editMenu.add_command(label="Grayscale", command=lambda: self.grayscale("Grayscale"), state='disabled')
 
-        self.imageBrighteningMenu.add_command(label="Addition with Scalar", command = lambda: self.scalar_input_window("Addition with Scalar"))
-        self.imageBrighteningMenu.add_command(label="Multiplication with Scalar", command = lambda: self.scalar_input_window("Multiplication with Scalar"))
+        self.imageBrighteningMenu.add_command(label="Addition with Scalar", command = lambda: self.scalar_input_window("Addition with Scalar"), state='disabled')
+        self.imageBrighteningMenu.add_command(label="Multiplication with Scalar", command = lambda: self.scalar_input_window("Multiplication with Scalar"), state='disabled')
         self.editMenu.add_cascade(label="Image Brightening", menu=self.imageBrighteningMenu)
 
-        self.arithmeticsMenu.add_command(label="Addition with Image", command = lambda: self.open_image("Addition with Image"))
-        self.arithmeticsMenu.add_command(label="Subtraction with Image", command =lambda: self.open_image("Subtraction with Image"))
+        self.arithmeticsMenu.add_command(label="Addition with Image", command = lambda: self.open_image("Addition with Image"), state='disabled')
+        self.arithmeticsMenu.add_command(label="Subtraction with Image", command =lambda: self.open_image("Subtraction with Image"), state='disabled')
         self.editMenu.add_cascade(label="Arithmetics", menu=self.arithmeticsMenu)
 
-        self.booleanMenu.add_command(label="And with Image", command = lambda: self.open_image("And with Image"))
-        self.booleanMenu.add_command(label="Not with Image", command =lambda: self.open_image("Not with Image"))
-        self.booleanMenu.add_command(label="Or with Image", command = lambda: self.open_image("Or with Image"))
-        self.booleanMenu.add_command(label="Xor with Image", command =lambda: self.open_image("Xor with Image"))
+        self.booleanMenu.add_command(label="And with Image", command = lambda: self.open_image("And with Image"), state='disabled')
+        self.booleanMenu.add_command(label="Not with Image", command =lambda: self.open_image("Not with Image"), state='disabled')
+        self.booleanMenu.add_command(label="Or with Image", command = lambda: self.open_image("Or with Image"), state='disabled')
+        self.booleanMenu.add_command(label="Xor with Image", command =lambda: self.open_image("Xor with Image"), state='disabled')
         self.editMenu.add_cascade(label="Boolean", menu=self.booleanMenu)
 
-        self.geometryMenu.add_command(label="Translation", command = lambda: self.open_image("Translation"))
-        self.geometryMenu.add_command(label="Rotation", command =lambda: self.open_image("Rotation"))
-        self.geometryMenu.add_command(label="Flip", command = lambda: self.open_image("Flip"))
-        self.geometryMenu.add_command(label="Zoom", command =lambda: self.open_image("Zoom"))
+        self.geometryMenu.add_command(label="Translation", command = lambda: self.open_image("Translation"), state='disabled')
+
+        self.rotationMenu.add_command(label="Rotation 90 Clockwise", command = lambda: self.open_image("Rotation 90 Clockwise"), state='disabled')
+        self.rotationMenu.add_command(label="Rotation 90 Counter Clockwise", command =lambda: self.open_image("Rotation 90 Counter Clockwise"), state='disabled')
+        self.geometryMenu.add_cascade(label="Rotation", menu=self.rotationMenu)
+
+        self.flipMenu.add_command(label="Flip Vertical", command = lambda: self.open_image("Flip Vertical"), state='disabled')
+        self.flipMenu.add_command(label="Flip Horizontal", command =lambda: self.open_image("Flip Horizontal"), state='disabled')
+        self.geometryMenu.add_cascade(label="Flip", menu=self.flipMenu)
+
+        self.zoomMenu.add_command(label="Zoom In", command = lambda: self.open_image("Zoom In"), state='disabled')
+        self.zoomMenu.add_command(label="Zoom Out", command =lambda: self.open_image("Zoom Out"), state='disabled')
+        self.geometryMenu.add_cascade(label="Zoom", menu=self.zoomMenu)
+
         self.editMenu.add_cascade(label="Geometry", menu=self.geometryMenu)
 
         self.menubar.add_cascade(label="Edit", menu=self.editMenu)
 
-        self.histogramMenu.add_command(label="Show Histogram", command =lambda: self.open_image("Show Histogram"))
+        self.histogramMenu.add_command(label="Show Histogram", command =lambda: self.open_image("Show Histogram"), state='disabled')
+        self.histogramMenu.add_command(label="Show Normalized Histogram", command =lambda: self.open_image("Show Normalized Histogram"), state='disabled')
 
         self.menubar.add_cascade(label="Histogram", menu=self.histogramMenu)
         self.value.pack()
@@ -84,10 +99,14 @@ class App(Frame):
             "Or with Image" : 10,
             "Xor with Image" : 11,
             "Translation" : 12,
-            "Rotation" : 13,
-            "Flip" : 14,
-            "Zoom" : 15,
-            "Show Histogram" : 16,
+            "Rotation 90 Clockwise": 13,
+            "Rotation 90 Counter Clockwise": 14,
+            "Flip Vertical" : 14,
+            "Flip Horizontal" : 15,
+            "Zoom In" : 16,
+            "Zoom Out" : 17,
+            "Show Histogram" : 18,
+            "Show Normalized Histogram" : 19,
         } 
         idTemp = switcher.get(command, lambda: -1)
         if (not idTemp == -1):
@@ -102,39 +121,24 @@ class App(Frame):
 
     def open_image(self, command):
         self.idHandler(command)
-        if (self.id == 0):
-            #Addition with Image
-            print('0')
-        elif (self.id == 1):
-            #Subtraction with Image
-            print('1')
-        elif (self.id == 2):
-            #And with Image
-            print('2')
-        elif (self.id == 3):
-            #Not with Image
-            print('3')
-        elif (self.id == 4):
-            #Or with Image
-            print('4')
-        elif (self.id == 5):
-            #Xor with Image
-            print('5')
-        else:
+        # # Select the Imagename from a folder
+        print('id')
+        print(self.id.get()) 
+        x = self.open_filename()
+        self.rawImg = Image.open(x)
+
+        if (self.id.get() == 0):
             #Open Image at the beginning
             # # Select the Imagename from a folder 
-            x = self.open_filename()
-
-            self.rawImg = Image.open(x)
-            self.npArrImg = np.array(self.rawImg)
-            self.listImg = self.npArrImg.tolist()
-            self.imgType = ''
-            if (self.rawImg.mode == '1'):
-                self.imgType = 'BINARY'
-            elif (self.rawImg.mode == 'L'):
-                self.imgType = 'GRAYSCALE'
-            elif (self.rawImg.mode == 'RGB'):
-                self.imgType = "RGB"
+            # self.npArrImg = np.array(self.rawImg)
+            # self.listImg = self.npArrImg.tolist()
+            # self.imgType = ''
+            # if (self.rawImg.mode == '1'):
+            #     self.imgType = 'BINARY'
+            # elif (self.rawImg.mode == 'L'):
+            #     self.imgType = 'GRAYSCALE'
+            # elif (self.rawImg.mode == 'RGB'):
+            #     self.imgType = "RGB"
             #IMPORTANT
             #lempar array ke backend
             #self.imgObject = ip.makeImage(self.listImg, self.imgType)
@@ -145,21 +149,21 @@ class App(Frame):
             # imageType = 'RGB'
             # grayLevel = 255
             # if (imageType == 'RGB'):
-            #     self.imageArr = np.zeros((h, w, 3), dtype=np.uint8)
-            #     self.imageArr[0:256, 0:256] = [255, 0, 0] # red patch in upper left
-            #     self.rawImg = Image.fromarray(self.imageArr, 'RGB')
+            #     self.imageArrMain = np.zeros((h, w, 3), dtype=np.uint8)
+            #     self.imageArrMain[0:256, 0:256] = [255, 0, 0] # red patch in upper left
+            #     self.rawImg = Image.fromarray(self.imageArrMain, 'RGB')
             # elif (imageType == 'GRAYSCALE'):
             #     if (grayLevel < 255):
             #         #yang ini belom, kyknya pake matplotlib ajadeh wkkw
             #         print('aaa')
             #     else:
-            #         self.imageArr = np.zeros((h, w), dtype=np.uint8)
-            #         self.imageArr[0:256, 0:256] = [125] # red patch in upper left
-            #         self.rawImg = Image.fromarray(self.imageArr, 'P')
+            #         self.imageArrMain = np.zeros((h, w), dtype=np.uint8)
+            #         self.imageArrMain[0:256, 0:256] = [125] # red patch in upper left
+            #         self.rawImg = Image.fromarray(self.imageArrMain, 'P')
             # elif (imageType == 'BINARY'):
-            #     self.imageArr = np.zeros((h, w), dtype=np.uint8)
-            #     self.imageArr[0:256, 0:256] = [1] # red patch in upper left
-            #     self.rawImg = Image.fromarray(self.imageArr, '1')
+            #     self.imageArrMain = np.zeros((h, w), dtype=np.uint8)
+            #     self.imageArrMain[0:256, 0:256] = [1] # red patch in upper left
+            #     self.rawImg = Image.fromarray(self.imageArrMain, '1')
 
             # PhotoImage class is used to add image to widgets, icons etc 
             self.img = ImageTk.PhotoImage(self.rawImg) 
@@ -172,8 +176,13 @@ class App(Frame):
             width, height = self.rawImg.size
             self.statusbar.config(text=self.filename + ' height = ' + str(height) + ' width = ' + str(width) + ' format = ' + str(self.rawImg.format) + ' size = ' + str(os.path.getsize(self.filename)) + ' bytes')
             self.disable_sub_menu('Open')
-            self.enable_sub_menu('Save File')
-    
+            
+        else:
+            self.imageArrOperation = np.array(self.rawImg)
+            print(self.imageArrOperation.shape)
+            #panggil fungsi yang ngelakuin operasi2nya di backend
+        self.enable_sub_menu('Save File')
+        self.enable_all_sub_menu()
 
     def save_image(self, command):
         self.idHandler(command) 
@@ -194,16 +203,35 @@ class App(Frame):
         self.rawImg.save(file)
 
     def enable_sub_menu(self, menu):
-        if (menu == 'Open'):
-            self.fileMenu.entryconfig('Open', state='normal')
-        elif (menu == 'Save File'):
-            self.fileMenu.entryconfig('Save File', state='normal')
+        # if (menu == 'Open'):
+        #     self.fileMenu.entryconfig('Open', state='normal')
+        # elif (menu == 'Save File'):
+        #     self.fileMenu.entryconfig('Save File', state='normal')
+        self.fileMenu.entryconfig(menu, state='normal')
 
     def disable_sub_menu(self, menu):
-        if (menu == 'Open'):
-            self.fileMenu.entryconfig('Open', state='disabled')
-        elif (menu == 'Save File'):
-            self.fileMenu.entryconfig('Save File', state='disabled')
+        # if (menu == 'Open'):
+        #     self.fileMenu.entryconfig('Open', state='disabled')
+        # elif (menu == 'Save File'):
+        #     self.fileMenu.entryconfig('Save File', state='disabled')
+        self.fileMenu.entryconfig(menu, state='disabled')
+
+    def enable_all_sub_menu(self):
+        self.editMenu.entryconfig("Negative", state='normal')
+        self.editMenu.entryconfig("Grayscale", state='normal')
+        self.imageBrighteningMenu.entryconfig("Addition with Scalar", state='normal')
+        self.imageBrighteningMenu.entryconfig("Multiplication with Scalar", state='normal')
+        self.arithmeticsMenu.entryconfig("Addition with Image", state='normal')
+        self.arithmeticsMenu.entryconfig("Subtraction with Image", state='normal')
+        self.booleanMenu.entryconfig("And with Image", state='normal')
+        self.booleanMenu.entryconfig("Not with Image", state='normal')
+        self.booleanMenu.entryconfig("Or with Image", state='normal')
+        self.booleanMenu.entryconfig("Xor with Image", state='normal')
+        self.geometryMenu.entryconfig("Translation", state='normal')
+        self.geometryMenu.entryconfig("Rotation", state='normal')
+        self.geometryMenu.entryconfig("Flip", state='normal')
+        self.geometryMenu.entryconfig("Zoom", state='normal')
+        self.histogramMenu.entryconfig("Show Histogram", state='normal')
 
     def negative(self, command):
         # ubah image object jadi negative
@@ -241,58 +269,8 @@ class App(Frame):
     
     def ok(self):
         #panggil fungsi image brightening backend
-        # if (self.id == 0):
-        #     print('abc')
-        # elif (self.id == 1):
-        #     print('def')
-        # self.scalarInputWindow.withdraw()
-        # self.parent.update()
-        # self.parent.deiconify()
-        # scalar = self.
         self.strVar.set(str(int(self.scalarValue.get())))
         self.scalarInputWindow.destroy()
-
-
-
-# class ImageBrightenerWindow(Frame):
-#     def __init__(self, parent):
-#         Frame.__init__(self, parent)
-#         self.parent = parent
-#         self.scalarInputWindow = Toplevel(self.parent) 
-#         self.scaleLabel = Label(self.scalarInputWindow)
-#         self.brighteningScaleAdd = DoubleVar()
-#         self.id = 0 #id 0 untuk additional, id 2 untuk multiplication
-#         self.initUI()
-
-#     def initUI(self):
-#         # sets the title of the Toplevel widget 
-#         self.scalarInputWindow.title("Scalar Value") 
-    
-#         # sets the geometry of toplevel 
-#         self.scalarInputWindow.geometry("200x200")
-#         scale = Scale(self.scalarInputWindow, variable=self.brighteningScaleAdd, from_=-255, to=255, orient=HORIZONTAL)
-#         showScaleButton = Button(self.scalarInputWindow, text ="Show Scale", command = self.show)
-#         okButton = Button(self.scalarInputWindow, text ="OK", command = self.ok)
-        
-#         scale.pack()
-#         showScaleButton.pack()
-#         okButton.pack()
-#         self.scaleLabel.pack()
-
-#     def show(self):
-#         selection = "Value = " + str(int(self.brighteningScaleAdd.get()))
-#         self.scaleLabel.config(text = selection)
-    
-#     def ok(self):
-#         #panggil fungsi image brightening backend
-#         if (self.id == 0):
-#             print('abc')
-#         elif (self.id == 1):
-#             print('def')
-#         self.scalarInputWindow.withdraw()
-#         self.parent.update()
-#         self.parent.deiconify()
-
 
 def main():
     root = Tk()
